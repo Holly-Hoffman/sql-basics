f8beb9e8c647c5b12f34225b40c0df7537577489
Holly Hoffman <1writecandidate@gmail.com>
2024-08-21 16:07:08 -0400

    First Updates

--- ch2-howto.sql
+++ ch2-howto.sql
@@ -1,2 +1,21 @@
 --Chapter 2 Basic Statements
 SELECT*FROM Vendors;
+
+-- Chapter 3 pg 65
+SELECT*
+	FROM Invoices;
+-- same query but only show 3 columns and order by invoice total
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
+	FROM Invoices
+		ORDER BY InvoiceTotal;
+
+-- two columns and calculated value for specific invoice of ID 17
+SELECT InvoiceID, InvoiceTotal, CreditTotal + PaymentTotal AS TotalCredits
+FROM Invoices
+WHERE InvoiceID = 17;
+
+-- get all invoices between given dates
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
+FROM Invoices
+WHERE InvoiceDate BETWEEN '2023-01-01' and '2023-03-31'
+ORDER BY InvoiceDate;
