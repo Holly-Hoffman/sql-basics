e46079ead8d186c0c13c20d1c37be9b20d019842
Holly Hoffman <1writecandidate@gmail.com>
2024-08-21 16:24:07 -0400

    Second Updates

--- ch2-howto.sql
+++ ch2-howto.sql
@@ -19,3 +19,30 @@ SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
 FROM Invoices
 WHERE InvoiceDate BETWEEN '2023-01-01' and '2023-03-31'
 ORDER BY InvoiceDate;
+
+-- something that gives empty returns
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
+FROM Invoices
+WHERE InvoiceTotal > 50000;
+
+-- page 69
+-- Naming columns in the results set
+SELECT InvoiceNumber AS [Invoice Number], InvoiceDate AS Date, InvoiceTotal AS Total
+FROM Invoices;
+
+-- doesn't provide name for calculated column
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal, InvoiceTotal - PaymentTotal - CreditTotal
+FROM Invoices;
+
+-- pg 71 coding expressions
+-- concatenating string data
+SELECT VendorCity, VendorState, VendorCity + VendorState
+FROM Vendors;
+
+-- making it pretty
+SELECT VendorCity, VendorState, VendorCity + ', ' + VendorState + ' ' + VendorZipCode AS Address
+FROM Vendors;
+
+-- with apostrophes
+SELECT VendorName + '''s Address: ', VendorCity + ', ' + VendorState + ' ' + VendorZipCode AS Address
+FROM Vendors;
