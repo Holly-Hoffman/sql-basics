6ecded475f92cd60143e43e3bdca4b6134e04458
Holly Hoffman <1writecandidate@gmail.com>
2024-08-26 14:04:49 -0400

    adding prsdb insert.

--- bmdb-insert.sql
+++ bmdb-insert.sql
@@ -52,6 +52,16 @@ VALUES
 	('William', 'Sadler', 'M', '1950-04-13'),
 	('Clancy', 'Brown', 'M', '1959-01-05');
 
+INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
+VALUES
+	('Gil', 'Bellows', 'M', '1967-06-28'),
+	('Mark', 'Rolston', 'M', '1956-12-07'),
+	('Larry', 'Brandenburg', 'M', '1955-07-10'),
+	('James', 'Whitmore', 'M', '1921-10-01'),
+	('Jeff', 'DeMunn', 'M', '1947-04-25'),
+	('Neil', 'Giuntoli', 'M', '1959-12-20'),
+	('Brian', 'Libby', 'M', '1949-04-20');
+
 --inserting more into the credits (TSR)
 INSERT INTO Credit (MovieId, ActorID, Role)
 VALUES
@@ -59,6 +69,16 @@ VALUES
 	(1, 3, 'Warden Norton'),
 	(1, 4, 'Heywood'),
 	(1, 5, 'Captain Hadley');
+
+INSERT INTO Credit (MovieId, ActorID, Role)
+VALUES
+	(1, 37, 'Tommy'),
+	(1, 38, 'Bogs Diamond'),
+	(1, 39, 'Skeet'),
+	(1, 40, 'Brooks Hatlen'),
+	(1, 41, '1949 DA'),
+	(1, 42, 'Jigger'),
+	(1, 43, 'Floyd');
 
 --inserting more into the actors (CA)
 INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
@@ -69,6 +89,38 @@ VALUES
 	('Hugo', 'Weaving', 'M', '1960-04-04'),
 	('Jim', 'Sturgess', 'M', '1978-05-16');
 
+INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
+VALUES
+	('Susan', 'Sarandon', 'F', '1946-10-04'),
+	('Ben', 'Whishaw', 'M', '1980-10-14'),
+	('Doona', 'Bae', 'F', '1979-10-11'),
+	('James', 'D’Arcy', 'M', '1975-08-27'),
+	('Keith', 'David', 'M', '1956-06-04'),
+	('Zhao', 'Wei', 'F', '1976-03-12'),
+	('Ben', 'Wishaw', 'M', '1980-10-14'),
+	('Hugh', 'Grant', 'M', '1960-09-09'),
+	('David', 'Gyasi', 'M', '1975-01-03'),
+	('Laura', 'Murray', 'F', '1979-04-23'),
+	('Robert', 'Forster', 'M', '1941-07-13'),
+	('Nina', 'Sewell', 'F', '1984-08-17'),
+	('Martin', 'Woolf', 'M', '1981-05-25'),
+	('Cesar', 'Gonzalez', 'M', '1984-10-22'),
+	('Van', 'Tuyen', 'F', '1980-03-30'),
+	('Isabella', 'Corbellini', 'F', '1978-12-12');
+
+UPDATE Actor
+SET FirstName = 'Xun',
+LastName = 'Zhou'
+WHERE Id = 26;
+
+UPDATE Actor
+SET FirstName = 'Wen',
+LastName = 'Jiang'
+WHERE Id = 15;
+
+DELETE FROM Actor
+WHERE Id > 29;
+
 --Inserting more into the credits (CA)
 INSERT INTO Credit (MovieId, ActorID, Role)
 VALUES
@@ -77,6 +129,18 @@ VALUES
 	(2, 8, 'Captain Molyneux / Vyvyan Arys / Timothy Cavendish / Korean Musician / Prescient 2'),
 	(2, 9, 'Haskell Moore / Tadeusz Kesselring / Bill Smoke / Nurse Noakes / Boardman Mephi / Old Georgie'),
 	(2, 10, 'Adam Ewing / Poor Hotel Guest / Megans Dad / Highlander / Hae-Joo Chang / Adam');
+
+INSERT INTO Credit (MovieId, ActorID, Role)
+VALUES
+	(2, 21, 'Madame Horrox / Older Ursula / Yusouf Suleiman / Abbess'),
+	(2, 22, 'Cabin Boy / Robert Frobisher / Store Clerk / Georgette / Tribesman'),
+	(2, 23, 'Tilda / Megans Mom / Mexican Woman / Sonmi-451 / Sonmi-351 / Sonmi Prostitute'),
+	(2, 24, 'Young Rufus Sixsmith / Old Rufus Sixsmith / Nurse James / Archivist'),
+	(2, 25, 'Kupaka / Joe Napier / An-kor Apis / Prescient'),
+	(2, 26, 'Talbot (Hotel Manager) / Yoona-939 / Rose'),
+	(2, 27, 'Cabin Boy / Robert Frobisher / Store Clerk / Georgette / Tribesman'),
+	(2, 28, 'Rev. Giles Horrox / Hotel Heavy / Lloyd Hooks / Denholme Cavendish / Seer Rhee / Kona Chief'),
+	(2, 29, 'Autua / Lester Rey / Duophysite');
 
 --Inserting more into the actors (RO)
 INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
@@ -87,6 +151,18 @@ VALUES
 	('Donnie', 'Yen', 'M', '1963-07-27'),
 	('Wen', 'Jiang', 'M', '1963-01-05');
 
+INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
+VALUES
+	('Ben', 'Mendelsohn', 'M', '1969-04-03'),
+	('Forest', 'Whitaker', 'M', '1961-07-15'),
+	('Mads', 'Mikkelsen', 'M', '1965-11-22'),
+	('Jiang', 'Wen', 'F', '1965-01-26'),
+	('Riz', 'Ahmed', 'M', '1982-12-01'),
+	('Guy', 'Henry', 'M', '1960-10-17'),
+	('Genevieve', 'O’Reilly', 'F', '1974-01-06'),
+	('Jimmy', 'Smits', 'M', '1955-07-09'),
+	('Alistair', 'Petrie', 'M', '1970-09-30');
+
 --Inserting more into the credits (RO)
 INSERT INTO Credit (MovieId, ActorID, Role)
 VALUES
@@ -96,6 +172,21 @@ VALUES
 	(4, 14, 'Chirrut Imwe'),
 	(4, 15, 'Blaze Malbus');
 
+INSERT INTO Credit (MovieId, ActorID, Role)
+VALUES
+	(4, 44, 'Orson Krennic'),
+	(4, 45, 'Saw Gerrera'),
+	(4, 46, 'Galen Erso'),
+	(4, 48, 'Bodhi Rook'),
+	(4, 49, 'Governor Tarkin'),
+	(4, 50, 'Mon Mothma'),
+	(4, 51, 'Bail Organa'),
+	(4, 52, 'General Draven');
+
+DELETE FROM Actor
+WHERE Id = 47;
+
+
 --Inserting more into the actors (LotR)
 INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
 VALUES
@@ -104,6 +195,19 @@ VALUES
 	('Billy', 'Boyd', 'M', '1968-08-28'),
 	('Viggo', 'Mortensen', 'M', '1958-10-20'),
 	('Orlando', 'Bloom', 'M', '1977-01-13');
+
+INSERT INTO Actor (FirstName, LastName, Gender, Birthdate)
+VALUES
+	('Elijah', 'Wood', 'M', '1981-01-28'),
+	('Ian', 'McKellen', 'M', '1939-05-25'),
+	('Cate', 'Blanchett', 'F', '1969-05-14'),
+	('Liv', 'Tyler', 'F', '1977-07-01'),
+	('John', 'Rhys-Davies', 'M', '1944-05-05'),
+	('Christopher', 'Lee', 'M', '1922-05-27'),
+	('Hugo', 'Weaving', 'M', '1960-04-04'),
+	('Andy', 'Serkis', 'M', '1964-04-20'),
+	('Dominic', 'Monaghan', 'M', '1976-12-08'),
+
 
 --Inserting more into the credits (LotR)
 INSERT INTO Credit (MovieId, ActorID, Role)
--- prsdb-create-sqlsvr.sql
+++ prsdb-create-sqlsvr.sql
@@ -12,63 +12,127 @@ USE PRSDB;
 GO
 
 CREATE TABLE Users (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	Username varchar(255) NOT NULL,
-	Password varchar(255) NOT NULL,
-	FirstName varchar(255) NOT NULL,
-	LastName varchar(255) NOT NULL,
-	PhoneNumber varchar(25) NOT NULL,
-	Email varchar(255) NOT NULL,
-	Reviewer char(1) NOT NULL,
-	Admin char(1) NOT NULL,
-	CONSTRAINT UQ_User_Name UNIQUE (Username),
-	CONSTRAINT UQ_Person UNIQUE (Firstname, LastName, PhoneNumber),
-	CONSTRAINT UQ_Email UNIQUE (Email)
+	Id				int				PRIMARY KEY IDENTITY(1,1),
+	Username		varchar(20)		NOT NULL,
+	Password		varchar(10)		NOT NULL,
+	FirstName		varchar(20)		NOT NULL,
+	LastName		varchar(20)		NOT NULL,
+	PhoneNumber		varchar(12)		NOT NULL,
+	Email			varchar(75)		NOT NULL,
+	Reviewer		bit				default 0 NOT NULL,
+	Admin			bit				default 0 NOT NULL,
+	CONSTRAINT UQ_User_UserName		UNIQUE (Username),
+	CONSTRAINT UQ_Person			UNIQUE (Firstname, LastName, PhoneNumber),
+	CONSTRAINT UQ_User_Email		UNIQUE (Email)
 );
 
 CREATE TABLE Request (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	UserId int NOT NULL,
-	Description varchar(255) NOT NULL,
-	Justification varchar(255) NOT NULL,
-	DateNeeded date,
-	DeliveryMode varchar(50) NOT NULL,
-	Status varchar(50) NOT NULL,
-	Total decimal(20,2) NOT NULL,
-	SubmittedDate smalldatetime NOT NULL,
-	ReasonForRejection varchar(255),
-	FOREIGN KEY (UserId) REFERENCES Users(Id)
+	Id					int				PRIMARY KEY IDENTITY(1,1),
+	UserId				int				NOT NULL,
+	Description			varchar(100)	NOT NULL,
+	Justification		varchar(255)	NOT NULL,
+	DateNeeded			date			NULL,
+	DeliveryMode		varchar(25)		NOT NULL,
+	Status				varchar(20)		default 'New' NOT NULL,
+	Total				decimal(10,2)	NOT NULL,
+	SubmittedDate		datetime		default current_timestamp NOT NULL,
+	ReasonForRejection	varchar(100)	NULL,
+	FOREIGN KEY (UserId)				REFERENCES Users(Id)
 );
 
 CREATE TABLE Vendor (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	Code varchar(255) NOT NULL,
-	Name varchar(255) NOT NULL,
-	Address varchar(255) NOT NULL,
-	City varchar(255) NOT NULL,
-	State char(2),
-	Zip varchar(10) NOT NULL,
-	PhoneNumber varchar(25) NOT NULL,
-	Email varchar(255) NOT NULL,
-	CONSTRAINT UQ_Vendor UNIQUE (Name, Address, City, State)
+	Id				int				PRIMARY KEY IDENTITY(1,1),
+	Code			varchar(10)		NOT NULL,
+	Name			varchar(255)	NOT NULL,
+	Address			varchar(255)	NOT NULL,
+	City			varchar(255)	NOT NULL,
+	State			char(2)			NOT NULL,
+	Zip				char(5)			NOT NULL,
+	PhoneNumber		varchar(12)		NOT NULL,
+	Email			varchar(100)	NOT NULL,
+	CONSTRAINT UQ_Vendor_Business	UNIQUE (Name, Address, City, State),
+	CONSTRAINT UQ_Vendor_Code		UNIQUE (Code)
 );
 
 CREATE TABLE Products (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	VendorId int NOT NULL,
-	PartNumber varchar(255) NOT NULL,
-	Name varchar(255) NOT NULL,
-	Price decimal(10,2) NOT NULL,
-	Unit varchar(255) NOT NULL,
-	CONSTRAINT UQ_PartNum UNIQUE (PartNumber),
-	FOREIGN KEY (VendorId) REFERENCES Vendor(Id)
+	Id				int				PRIMARY KEY IDENTITY(1,1),
+	VendorId		int				NOT NULL,
+	PartNumber		varchar(50)		NOT NULL,
+	Name			varchar(150)	NOT NULL,
+	Price			decimal(10,2)	NOT NULL,
+	Unit			varchar(255)	NULL,
+	PhotoPath		varchar(255)	NULL,
+	CONSTRAINT UQ_Vendor_Part		UNIQUE (VendorId, PartNumber),
+	FOREIGN KEY (VendorId)			REFERENCES Vendor(Id)
 );
 
 CREATE TABLE LineItem (
-	Id int PRIMARY KEY IDENTITY(1,1),
-	RequestId int NOT NULL,
-	ProductID int NOT NULL,
-	Quantity int NOT NULL,
-	FOREIGN KEY (RequestId) REFERENCES Request(Id),
-	FOREIGN KEY (ProductId) REFERENCES Products(Id)
+	Id				int				PRIMARY KEY IDENTITY(1,1),
+	RequestId		int				NOT NULL,
+	ProductID		int				NOT NULL,
+	Quantity		int				NOT NULL,
+	FOREIGN KEY (RequestId)			REFERENCES Request(Id),
+	FOREIGN KEY (ProductId)			REFERENCES Products(Id),
+	CONSTRAINT UQ_Product_Request	UNIQUE (RequestId, ProductId)
 );
+
+--Inserting some users
+INSERT INTO Users (Username, Password, FirstName, LastName, PhoneNumber, Email, Reviewer, Admin)
+VALUES
+	('Boomerang Guy', 'p0nytail!', 'Sokka', 'Water Tribe', '555-5555', 'WangFire@watertribe.net', 1, 1),
+	('Sifu Katara', 'DeadM0m', 'Katara', 'Water Tribe', '555-5555', 'SaphhireFire@watertribe.net', 1, 0),
+	('The Avatar', 'Gyatso', 'Aang', 'the Avatar', '867-5309', 'Kuzon@airnomads.com', 0, 1),
+	('TheBestBender', 'metal4lyfe', 'Toph', 'Beifong', '123-4567', 'BlindBandit@earth.kingdom.com', 0, 0),
+	('FireLord', 'scarface', 'Fire Lord', 'Zuko', '320-4352', 'mommasboy@firenation.net', 1, 1);
+
+--Inserting some vendors
+INSERT INTO Vendor (Code, Name, Address, City, State, Zip, PhoneNumber, Email)
+VALUES
+	('0000010101', 'Varrick Global Industries', '171 Harbour Way', 'Republic City', 'EK', '56782', '623-5467', 'VGI@republic.com'),
+	('0000020101', 'Future Industries', '170 Sato Street', 'Republic City', 'EK', '56728', '523-5617', 'SatoFutureIndustries@republic.com'),
+	('0000010201', 'Cabbage Corp', '120 Leafy Lane', 'Ba Sing Se', 'EK', '23486', '513-5852', 'mycabbages@market.net'),
+	('0000010102', 'Fifth Nation', '689 Pirate Bay', 'South Pole', 'WT', '68422', '666-6666', 'curios@mobile.net'),
+	('0000010103', 'Fire Nation Circus', '102 Lion Vulture Drive', 'Ember Island', 'FN', '75395', '755-3685', 'ShuExotic@bigtent.com');
+
+--Inserting some products
+INSERT INTO Products (VendorId, PartNumber, Name, Price, Unit, PhotoPath)
+VALUES
+	(1, 'AV1', 'Satomobile Car', 15000.99, NULL, NULL),
+	(2, 'AV1', 'Satomobile Car', 18000.99, NULL, NULL),
+	(3, 'AV1', 'Satomobile Car', 16999.99, NULL, NULL),
+	(4, 'CO1', 'Ruby-Eyed Monkey Statue', 500.00, NULL, NULL),
+	(5, 'EA1', 'Platypus Bear', 86.00, 'cub', NULL),
+	(4, 'EA1', 'Platypus Bear', 120.00, 'cub', NULL),
+	(3, 'FP1', 'Cabbage', 3.00, 'head', NULL),
+	(4, 'FP1', 'Cabbage', 2.00, 'head', NULL),
+	(5, 'FP1', 'Cabbage', 0.50, 'head', NULL),
+	(3, 'FP2', 'Cabbage', 30.00, 'crate', NULL),
+	(4, 'FP2', 'Cabbage', 20.00, 'crate', NULL),
+	(5, 'FP2', 'Cabbage', 5.00, 'crate', NULL),
+	(3, 'AV2', 'Cabbage Compact', 8000.99, NULL, NULL),
+	(2, 'AV2', 'Cabbage Compact', 10000.99, NULL, NULL),
+	(4, 'CO2', 'Waterbending Scroll', 200.00, NULL, NULL),
+	(1, 'CO3', 'Satomobile Air Freshener', 15.00, 'each', NULL),
+	(2, 'CO3', 'Satomobile Air Freshener', 13.00, 'each', NULL),
+	(3, 'CO3', 'Satomobile Air Freshener', 18.00, 'each', NULL),
+	(4, 'CO3', 'Satomobile Air Freshener', 28.00, 'each', NULL),
+	(1, 'CO4', 'Satomobile Air Freshener', 40.00, 'three pack', NULL),
+	(2, 'CO4', 'Satomobile Air Freshener', 35.00, 'three pack', NULL),
+	(3, 'CO4', 'Satomobile Air Freshener', 50.00, 'three pack', NULL),
+	(4, 'CO4', 'Satomobile Air Freshener', 80.00, 'three pack', NULL),
+	(1, 'AV3', 'Mecha Tank', 299.99, NULL, NULL),
+	(2, 'AV3', 'Mecha Tank', 299.99, NULL, NULL),
+	(3, 'AV3', 'Mecha Tank', 399.99, NULL, NULL),
+	(4, 'EA2', 'Sky Bison', 800.00, 'calf', NULL),
+	(5, 'EA2', 'Sky Bison', 820.00, 'calf', NULL),
+	(5, 'EA3', 'Sky Bison', 420.00, 'adult', NULL),
+	(4, 'EA3', 'Sky Bison', 400.00, 'adult', NULL);
+
+Select *
+FROM Vendor;
+
+Select *
+FROM Users;
+
+Select *
+FROM Products;
